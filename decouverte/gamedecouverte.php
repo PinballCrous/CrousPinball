5e42a61dbcf519e683a1ab0068a114f4b26ab331
Jason <jason13000@hotmail.fr>
2019-07-18 15:03:26 +0200

    charte graphique check pour le bouton menu

--- decouverte/gamedecouverte.php
+++ decouverte/gamedecouverte.php
@@ -58,8 +58,8 @@
                     <p class="definition" id="unive">Universitaires</p>
                     <p class="definition" id="scola">Scolaires</p>
                 </div>
-                <div>
-                    <button class="btn_menu"><a href="../defis/IndexDefis.php">Menu</a></button>
+                <div class="prebtn">
+                    <button class="btn_menu"><a href="../templates/index.php">Menu</a></button>
                 </div>
 
                 <!-- <audio src="musique/blaster.mp3" id="blaster"></audio>
