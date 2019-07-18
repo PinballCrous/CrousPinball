ef3c175f8fa5f48b73853f5c0d1754a188ba2003
Jason <jason13000@hotmail.fr>
2019-07-18 09:38:37 +0200

    creation dossier et page de base découverte et defis (problème avec les includes)

--- templates/defis/IndexDefis.php
+++ templates/defis/IndexDefis.php
@@ -2,28 +2,29 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <?php include 'includes/css.php'; ?>
-    <title>Document</title>
+    <?php require_once '../includes/css.php'; ?>
+    <title>Menu defis</title>
 </head>
 <body>
     <main>
         <div class="d-flex flex-column d-flex justify-content-center align-items-center vh-100">
             <div class="fixed-top justify-content-end row mr-1">
-                <a href="../formulaire/connexion.php" ><button type="button" class="btn btn-danger">Connexion</button></a>
-                <a href="../formulaire/inscription.php"<button type="button" class="btn btn-danger ">Inscription</button></a>
+                <a href="../../formulaire/connexion.php" ><button type="button" class="btn btn-danger">Connexion</button></a>
+                <a href="../../formulaire/inscription.php"><button type="button" class="btn btn-danger ">Inscription</button></a>
             </div>
 
             <h1 class="text-danger">PinballCrous</h1>
             <p>Bienvenue sur le FlipperCrous ici vous trouverez un jeu ludique pour mieux connaitre le crous</p>
             <div class="d-inline-block">
             <button type="button" class="btn btn-danger">Jouer une partie simple</button>
-            <a href="defis/indexDefis.php"><button type="button" class="btn btn-danger">Jouer une partie à thème</button></a>
+            <button type="button" class="btn btn-danger">Jouer une partie à thème</button>
+            <h1>Fait un truc jolie bordel!!!!!</h1>
         </div>
         </div>
     </main>
 
 
 
-    <?php include_once 'includes/js.php'; ?>
+    <?php require '../includes/js.php'; ?>
 </body>
 </html>
