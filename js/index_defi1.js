f3afc7d84b045790d9f8cc47a608646b6f435706
Icanor <rugbyivan21@gmail.com>
2019-07-19 12:28:19 +0200

    tentative ball

--- js/index_defi1.js
+++ js/index_defi1.js
@@ -486,9 +486,11 @@
 		levelScore = newCurrentLevel;
 		$levelScore.text(levelScore);
 		
-		if (levelCurrent=="10") { 
+		if (levelCurrent=="2") { 
+
 			
-			//alert("Saviez vous que grâce aux restos U, les étudiants issus de tous les milieux ont la possibilité de prendre à l’extérieur de chez eux un repas par jour pour 3,25 € ? C’est une véritable mission de service public, une aide matérielle financée par l’Etat à travers le Cnous et les Crous.");
+			
+			// alert("Saviez vous que grâce aux restos U, les étudiants issus de tous les milieux ont la possibilité de prendre à l’extérieur de chez eux un repas par jour pour 3,25 € ? C’est une véritable mission de service public, une aide matérielle financée par l’Etat à travers le Cnous et les Crous.");
 		 
 		
 		}
