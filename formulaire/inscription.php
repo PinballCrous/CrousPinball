df2701bff85b6827932bc6161422e4257c2b71cc
adam <adam.masserini-pro@outlook.com>
2019-07-22 10:35:58 +0200

    incription refonctionnnel

--- formulaire/inscription.php
+++ formulaire/inscription.php
@@ -17,36 +17,29 @@ session_start();
         <h1 class="text-danger">PinballCrous</h1>
         <p>Inscrivez-vous pour sauvegarder vos scores!!</p>
         <div class="d-inline-block">
-            <form>
+            <form method="post" action="../Controller/traitement_connexion_inscription.php">
                 <div class="form-group">
-                    <label for="pseudo">Pseudo</label>
-                    <input type="text" class="form-control" id="pseudo" placeholder="Entrez votre pseudo">
+                    <input type="text" class="form-control" id="pseudo" name="pseudo" placeholder="Entrez votre pseudo">
                 </div>
                 <div class="form-group">
-                    <label for="nom">Nom</label>
-                    <input type="text" class="form-control" id="nom" placeholder="Entrez votre nom">
+                    <input type="text" class="form-control" id="nom" name="nom" placeholder="Entrez votre nom">
                 </div>
                 <div class="form-group">
-                    <label for="nom">Prenom</label>
-                    <input type="text" class="form-control" id="nom" placeholder="Entrez votre nom">
+                    <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Entrez votre nom">
                 </div>
                 <div class="form-group">
-                    <label for="email1">Email</label>
-                    <input type="email" class="form-control" id="email1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input type="email" class="form-control" id="mail" name="mail" aria-describedby="emailHelp" placeholder="Entrez email">
                     <small id="emailHelp" class="form-text text-muted">Nous ne partagerons votre e-mail avec aucun tier.</small>
                 </div>
                 <div class="form-group">
-                    <label for="email2">Email confirmé</label>
-                    <input type="email" class="form-control" id="email2" placeholder="Confirmer votre email">
+                    <input type="email" class="form-control" id="mail2" name="mail2" placeholder="Confirmer votre email">
                 </div>
                 <div class="form-group">
-                    <label for="password1">Mot de passe</label>
-                    <input type="password" class="form-control" id="password1" aria-describedby="passwordHelp" placeholder="Mot de passe">
+                    <input type="password" class="form-control" id="mdp" name="mdp" aria-describedby="passwordHelp" placeholder="Mot de passe">
                     <small id="passwordHelp" class="form-text text-muted">Ne partagez votre mot de passe avec aucun tier.</small>
                 </div>
                 <div class="form-group">
-                    <label for="password2">Mot de passe confirmé</label>
-                    <input type="password" class="form-control" id="password2" placeholder="Confirmez votre mot de passe">
+                    <input type="password" class="form-control" id="mdp2" name="mdp2" placeholder="Confirmez votre mot de passe">
                 </div>
 
                 <button type="submit" name="forminscription" id="forminscription" class="btn btn-success">Submit</button>
