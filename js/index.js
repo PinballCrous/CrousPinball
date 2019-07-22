05479429a1f0e2c32d87c15b7d9208fbab256b5b
helloio <rugbyivan21@gmail.com>
2019-07-22 09:29:04 +0200

    mode découverte repareeeee

--- js/index.js
+++ js/index.js
@@ -137,7 +137,7 @@
 			wall(415, 511, 20, 160, COLOR.INNER),
 
 			// flipper walls (left, right);
-			wall(87, 624, 20, 145, COLOR.INNER, -0.96),
+			wall(89, 624, 20, 145, COLOR.INNER, -0.96),
 			wall(360, 624, 20, 145, COLOR.INNER, 0.96),
 
 			// aprons (left, right)
