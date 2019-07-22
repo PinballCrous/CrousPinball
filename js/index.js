182591dc3307d0acc879d12809ec08cba81d4c95
helloio <rugbyivan21@gmail.com>
2019-07-22 10:13:34 +0200

    reparation bug stick gauche mode decouverte

--- js/index.js
+++ js/index.js
@@ -137,7 +137,7 @@
 			wall(415, 511, 20, 160, COLOR.INNER),
 
 			// flipper walls (left, right);
-			wall(89, 624, 20, 145, COLOR.INNER, -0.96),
+			wall(87.5, 623, 20, 141, COLOR.INNER, -0.96),
 			wall(360, 624, 20, 145, COLOR.INNER, 0.96),
 
 			// aprons (left, right)
@@ -386,15 +386,15 @@
 		});
 
 		// mouse drag (god mode for grabbing pinball)
-		Matter.World.add(world, Matter.MouseConstraint.create(engine, {
-			mouse: Matter.Mouse.create(render.canvas),
-			constraint: {
-				stiffness: 0.2,
-				render: {
-					visible: false
-				}
-			}
-		}));
+		// Matter.World.add(world, Matter.MouseConstraint.create(engine, {
+		// 	mouse: Matter.Mouse.create(render.canvas),
+		// 	constraint: {
+		// 		stiffness: 0.2,
+		// 		render: {
+		// 			visible: false
+		// 		}
+		// 	}
+		// }));
 
 		
 		// keyboard paddle events
