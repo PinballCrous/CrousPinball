6040d7c763761fb0e1133d91af3d8ac01380904c
jason <jason13000@hotmail.fr>
2019-07-22 11:42:51 +0200

    ajout image et titre de menus défis

--- defis/IndexDefis.php
+++ defis/IndexDefis.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <?php include '../templates/includes/css.php'; ?>
+    <link rel="stylesheet" href="cssdefis/styledefis.css">
     <title>Menu defis</title>
 </head>
 <body>
@@ -14,31 +15,31 @@
 
         <article class="mt-5 d-flex flex-column justify-content-center align-items-center">
             <div id="particulier"><h2 class="mb-1 font-weight-bold mt-5 text-danger">Nos défis ludique </h2></div>
-            <div class="row p-3 justify-content-center w-100 m-0">
-                    <div class="col-lg-3 col-md-12 z-depth-4 p-0 m-3 pb-2">
-                        <h4 class="card-header danger-color-dark border border-0 white-text text-center mb-3"><strong>DEFI 1</strong></h4>
-                        <img src="cdgdg" alt="">
-                        <p>Defi11111111</p>
+            <div class="row p-3 justify-content-center w-100 m-0 container-fluid">
+                    <div class="col-lg-3 col-md-12 z-depth-4 p-0 m-3 pb-2 pt-0">
+                        <h4 class="card-header danger-color-dark border border-0 white-text text-center"><strong>Campus</strong></h4>
+                        <img src="../img/campus.jpg" alt="campus" class="w-100">
+                        <p>Ici les niveaux du campus</p>
                         <a href="../lvl/lvl1.php"><button type="button" class="btn text-white danger-color-dark">Jouer</button></a>
                     </div>
 
                     <div class="col-lg-3 col-md-12 z-depth-4 p-0 m-3 pb-2">
-                        <h4 class="card-header danger-color-dark border border-0 white-text text-center mb-3"><strong>Culture</strong></h4>
-                        <img src="cdgdg" alt="">
+                        <h4 class="card-header danger-color-dark border border-0 white-text text-center"><strong>Logement</strong></h4>
+                        <img src="../img/logment.jpg" alt="logement" class="w-100">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         <button type="button" class="btn text-white danger-color-dark">Jouer</button>
                     </div>
 
                     <div class="col-lg-3 col-md-12 z-depth-4 p-0 m-3 pb-2">
-                        <h4 class="card-header danger-color-dark border border-0 white-text text-center mb-3"><strong>Culture</strong></h4>
-                        <img src="cdgdg" alt="">
+                        <h4 class="card-header danger-color-dark border border-0 white-text text-center"><strong>Aides</strong></h4>
+                        <img src="../img/aide.jpg" alt="aide" class="w-100">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         <button type="button" class="btn text-white danger-color-dark">Jouer</button>
                     </div>
 
                     <div class="col-lg-3 col-md-12 z-depth-4 p-0 m-3 pb-2">
-                        <h4 class="card-header danger-color-dark border border-0 white-text text-center mb-3"><strong>Culture</strong></h4>
-                        <img src="cdgdg" alt="">
+                        <h4 class="card-header danger-color-dark border border-0 white-text text-center"><strong>Restauration</strong></h4>
+                        <img src="../img/nourriture.jpg" alt="nourriture" class="w-100">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         <button type="button" class="btn text-white danger-color-dark">Jouer</button>
                     </div>
